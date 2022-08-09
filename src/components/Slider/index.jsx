04c9e55862f1004fbf6edb45a004319629ea77ef
doge <110700029+tototi932@users.noreply.github.com>
2022-08-08 22:02:55 -0400

    Update index.jsx

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -6,7 +6,7 @@ const styles={margin:"1rem 0.5rem 2rem 0rem"}
 const ReactSlider  =()=>{
     return(
         <div style={styles}>
-            <Slider marks={{25:'25%',50:'50%',75:'75%',100:'100%'}} step={null} railStyle={{backgroundColor:"#fff"}} trackStyle={{backgroundColor:"#198be3"}}/>
+            <Slider marks={{25:'25%',50:'50%',75:'75%',100:'100%'}} step={null} railStyle={{backgroundColor:"#fff"}} trackStyle={{backgroundColor:"#FF00FF"}}/>
         </div>
     )
 }
