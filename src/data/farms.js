a2674fa672587c01a2c99714e91471d951bf781e
doge <110700029+tototi932@users.noreply.github.com>
2022-08-08 23:43:31 -0400

    Update farms.js

--- src/data/farms.js
+++ src/data/farms.js
@@ -4,30 +4,30 @@ import BNB from "../images/BNB.svg";
 import USDT from "../images/USDT.svg";
 export const data = [
     {
-        pair:"LKD-BUSD LP",
+        pair:"WOW100K-BUSD LP",
         apy:"24.03%",
         daily:"0.059%",
         tvl:"-",
         network:"PancakeSwap",
-        img1:LKD,
+        img1:100K,
         img2:BUSD
     },
      {
-        pair:"LKD-BNB LP",
+        pair:"WOW100K-BNB LP",
         apy:"143.25%",
         daily:"0.2438%",
         tvl:"-",
         network:"PancakeSwap",
-        img1:LKD,
+        img1:100K,
         img2:BNB
     },
      {
-        pair:"LKD-USDT LP",
+        pair:"WOW100K-USDT LP",
         apy:"124.91%",
         daily:"0.223%",
         tvl:"-",
         network:"BabySwap",
-        img1:LKD,
+        img1:100K,
         img2:USDT
     },
 ]
